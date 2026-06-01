20a7c05bccdf6613b2a5287c6f0eb765cb1a9fa3
Miupan <twelveap.miu@gmail.com>
2026-06-01 22:07:53 +0100

    Add text input func

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const btn = document.getElementById('go');
 
 btn.addEventListener('click', function() {
-    alert('Button clicked!');
+
+    //set trim() to avoid the problem of users inputting only spaces
+    const input = document.getElementById('input-text').value.trim();
+
+    if (!input) {
+        alert('Please enter some text to translate!');
+        return;
+    }   
+
+    console.log('Input text:', input);
+
 });
